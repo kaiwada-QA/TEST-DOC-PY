8661f74cffd5c85b9401697175798b656bd30d55
和田凱 <167733542+kaiwada-QA@users.noreply.github.com>
2024-08-09 15:31:27 +0000

    jkjkjk

--- my_api_project/app/main.py
+++ my_api_project/app/main.py
@@ -19,9 +19,37 @@ def check_db():
             password=os.getenv("MYSQL_PASSWORD")
         )
         if connection.is_connected():
-            return {"status": "sucess"}
+            return {"status": "success"}
     except Error as e:
         return {"status": "miss", "error": str(e)}
     finally:
         if connection.is_connected():
+            connection.close()
+
+@app.post("/insert_data")
+def insert_data(id:str,moji:str,sansyou:str):
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv("MYSQL_HOST"),
+            database=os.getenv("MYSQL_DATABASE"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD")
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            insert_query = """
+                INSERT INTO users (id,moji,sansyou) 
+                VALUES (%s, %s,%s)
+            """
+            record = (id, moji,sansyou)
+            cursor.execute(insert_query, record)
+            connection.commit()
+
+            return {"status": "success", "user_id": cursor.lastrowid}
+    except Error as e:
+        return {"status": "miss", "error": str(e)}
+    finally:
+        if connection.is_connected():
+            cursor.close()
             connection.close()
